5a9a15aaea00c81a77c591b2300b2aa874ce9f4f
James Bester <jamesthomasbester@gmail.com>
2022-03-31 20:31:15 +1100

    added array of popular cocktails for autocomplete

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,9 @@ const locationApi = {
     key: 'f40fdbed4ce6f49a21c55972b85d4f67',
     uri: 'https://api.openweathermap.org/data/2.5/'
 };
-var atuoCocktails = ['Martini', 'Old Fashioned', 'Margarita', 'Cosmopolitan',]
+//array of popular cocktails for search autocomplete
+var atuoCocktails = ['Martini', 'Old Fashioned', 'Margarita', 'Cosmopolitan', 'Negroni', 'Moscow Mule', 'Martini', 'Mojito', 'Whiskey Sour', 'French 75', 
+    'Manhattan', 'Spritz', 'Gimlet', 'Sazerac' ,"Pimm's Cup", 'Vesper', 'Mimosa', 'Tom Collins', 'Daiquiri', 'Dark & Stormy', 'Martinez',]
 
 //where query is the search request and param is the parameters
 async function searchRequest(query, param){
